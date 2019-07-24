049f16b3b1ef57a99527ee4d9bd756050c1fef94
giriharan <giriharan777@gmail.com>
2019-07-24 10:21:24 +0530

    updated read.me

--- src/read.js
+++ src/read.js
@@ -72,7 +72,17 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger' -->
+import logger from 'redux-logger';
+
+const middleware = applyMiddleware(logger, thunk);
+
+const createStoreWithMiddleware = (createStore(reducers, middleware));
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware}>
+	<App />
+</Provider>
+, document.getElementById('root')); -->
 
 
 
